14541f5bd631b5ab186993d5e85429a979e42f24
Oksana Semonenko <ok.semonenko@gmail.com>
2016-07-19 15:12:20 +0300

    :tada:  :construction:  Выполнена часть 3: блок иконок

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,10 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync");
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
+var rename = require("gulp-rename");
+
 
 gulp.task("style", function() {
     gulp.src("sass/style.scss")
@@ -22,6 +26,16 @@ gulp.task("style", function() {
         ]))
         .pipe(gulp.dest("css"))
         .pipe(server.reload({stream: true}));
+});
+
+gulp.task("symbols", function() {
+    return gulp.src("img/icons/*.svg")
+        .pipe(svgmin())
+        .pipe(svgstore({
+            inlineSvg: true
+        }))
+        .pipe(rename("symbols.svg"))
+        .pipe(gulp.dest("img"));
 });
 
 gulp.task("serve", ["style"], function() {
