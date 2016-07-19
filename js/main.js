5cb18742879c30827b5465d3a327580475add7c7
Oksana Semonenko <ok.semonenko@gmail.com>
2016-07-19 17:16:21 +0300

    :tada:  :construction:  Начало адаптации под мобильные устройства и планшеты, адаптация блока навигации

--- js/main.js
+++ js/main.js
@@ -98,3 +98,17 @@ var onBtnClick =function (evt) {
 };
 
 btn.addEventListener("click", onBtnClick);
+
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+
+navToggle.addEventListener('click', function() {
+	if (navMain.classList.contains('main-nav--closed')) {
+		navMain.classList.remove('main-nav--closed');
+		navMain.classList.add('main-nav--opened');
+	} else {
+		navMain.classList.add('main-nav--closed');
+		navMain.classList.remove('main-nav--opened');
+	}
+});
