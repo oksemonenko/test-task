0a5a8d93458d3051fbbcbb39b9e0677be40c7708
Oksana Semonenko <ok.semonenko@gmail.com>
2016-07-20 18:21:28 +0300

    :tada:  :construction:  Выполнена часть 5: адаптация под устройства

--- js/main.js
+++ js/main.js
@@ -112,3 +112,87 @@ navToggle.addEventListener('click', function() {
 		navMain.classList.remove('main-nav--opened');
 	}
 });
+
+
+
+var feedback = document.querySelector(".feedback__btn");
+
+var overlay = document.querySelector(".modal-overlay");
+var popup = document.querySelector(".modal-content");
+var form = popup.querySelector(".form");
+var close = popup.querySelector(".modal-content__close");
+
+//Функция, показывающая попап
+function showPopup() {
+	overlay.classList.add("modal-overlay--show");
+	popup.classList.add("modal-content--show");
+}
+
+//Функция, скрывающая попап
+function hidePopup() {
+	popup.classList.remove("modal-content--show");
+	overlay.classList.remove("modal-overlay--show");
+}
+
+//Добавляет обработчик события кнопке входа
+feedback.addEventListener("click", function(event) {
+	event.preventDefault();
+	showPopup();
+});
+
+//Добавляет обработчик события закрывающей кнопке
+close.addEventListener("click", function(event) {
+	event.preventDefault();
+	hidePopup();
+});
+
+
+//Обработчик события: закрытие попапа по нажатию клавиши esc
+window.addEventListener("keydown", function(event) {
+	if (event.keyCode === 27) {
+		if (popup.classList.contains("modal-content--show")) {
+			hidePopup();
+		}
+	}
+});
+
+var submit = form.querySelector(".form__btn");
+var text = form.querySelector(".form__text");
+var tel = form.querySelector(".form__tel");
+var email = form.querySelector(".form__email");
+var textarea = form.querySelector(".form__textarea");
+
+/**
+ * Дисейблит кнопку
+ */
+submit.setAttribute('disabled', true);
+
+var validateFields = function () {
+	if (text.value && textarea.value && (tel.value || email.value)) {
+		submit.removeAttribute('disabled');
+	} else {
+		submit.setAttribute('disabled', true);
+	}
+};
+
+text.oninput = textarea.oninput = tel.oninput = email.oninput = function () {
+	validateFields();
+};
+
+form.validate(
+		{
+			rules : {
+				username : {required : true, minlength: 2},
+				password : "required"
+			},
+			messages : {
+				username : {
+					required : "Введите ваше имя",
+					minlength : "Введите не менее, чем 2 символа."
+				},
+				password : "Введите пароль"
+			}
+		}
+);
+
+
